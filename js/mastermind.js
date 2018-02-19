86d9dd6769a5a7b58206b3bf01a145b0d665d990
Florian Boulestreau <mescomptes.flow@gmail.com>
2018-02-19 21:56:56 +0100

    get pawn

--- js/mastermind.js
+++ js/mastermind.js
@@ -1,9 +1,33 @@
 var mm = {
-	init: function() {
-		console.log('ototo');
-		$('mastermind').remove();
-		$('mastermind').append('<div>toto</div>');
-	}
+	pawnCount: 4,
+
+    /**
+     * Run mastermind
+     */
+    run: function ()
+    {
+        $('mastermind').html(this.getPawn());
+
+    },
+
+    /**
+     * Get a random HTML color
+     */
+    getRandomColor: function ()
+    {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    },
+
+    getPawn: function()
+    {
+        var pawn = $('<div>toto</div>');
+        return pawn.css({backgroundColor:this.getRandomColor()})
+    }
 }
 
-mm.init();
+mm.run();

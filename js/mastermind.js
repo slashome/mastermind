fe19aeb8169d7256571c53efd1610500ae59eddc
Florian Boulestreau <mescomptes.flow@gmail.com>
2018-02-20 09:32:21 +0100

    generate data

--- js/mastermind.js
+++ js/mastermind.js
@@ -1,13 +1,31 @@
 var mm = {
-	pawnCount: 4,
+	nbCol: 4,
+    nbColor: 6,
+    rows: 10,
+    secretCode: [],
+    colors: [],
 
     /**
      * Run mastermind
      */
     run: function ()
     {
-        $('mastermind').html(this.getPawn());
+        this.generateColors();
+        this.generateSecret();
+        console.log(this.secretCode);
+        $('mastermind').html(this.buildTable());
+    },
 
+    /**
+     * Build the mastermind table
+     */
+    buildTable: function()
+    {
+        var table = $('<div></div>');
+        for (var i = 0; i < this.rows; i++) {
+            table.append(this.buildRow(i));
+        }
+        return table;
     },
 
     /**
@@ -23,9 +41,38 @@ var mm = {
         return color;
     },
 
-    getPawn: function()
+    generateSecret: function()
     {
-        var pawn = $('<div>toto</div>');
+        for (var i = 0; i < this.nbCol; i++) {
+            var colorIndex = Math.floor(Math.random() * this.nbColor);
+            this.secretCode.push(colorIndex);
+        }
+    },
+
+    buildRow: function(tableId)
+    {
+        var row = $('<div data-tableId="'+tableId+'"></div>');
+        row.css({clear:'both'});
+        for (var i = 0; i < this.nbCol; i++) {
+            row.append(this.buildPawn(i));
+        }
+        return row;
+    },
+
+    /**
+     * Create the array of colors
+     */
+    generateColors: function ()
+    {
+        for (var i = 0; i < this.nbColor; i++) {
+            this.colors.push(this.getRandomColor());
+        }
+    },
+
+    buildPawn: function(rowId)
+    {
+        var pawn = $('<div class="pawn">x</div>');
+        pawn.css({float:'left'});
         return pawn.css({backgroundColor:this.getRandomColor()})
     }
 }
